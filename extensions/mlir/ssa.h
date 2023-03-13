255c3eca6619cb3d12e3af2490773e0317fbfef4
barinsim <barinsim@fit.cvut.cz>
2023-03-13 22:22:15 +0100

    Add ssa candidates gathering and refactor

--- extensions/mlir/ssa.h
+++ extensions/mlir/ssa.h
@@ -2,7 +2,8 @@
 #define BACKENDS_MLIR_SSA_H_
 
 
-#include <stack>
+#include <variant>
+#include <unordered_set>
 #include "ir/ir.h"
 #include "ir/visitor.h"
 #include "ir/dump.h"
@@ -25,37 +26,77 @@ bool isPrimitiveType(const IR::Type *type) {
 }
 
 
-class Declaration
+class GatherOutParamScalars : public Inspector, P4WriteContext
 {
-    const IR::IDeclaration* instance;
-    std::optional<std::vector<const IR::StructField*>> field;
+    class Builder {
+        std::unordered_set<const IR::IDeclaration*> decls;
+     public:
+        void add(const IR::IDeclaration* decl) { decls.insert(decl); }
+        std::unordered_set<const IR::IDeclaration*> get() const { return decls; }
+    };
+
+    Builder b;
+
+    const P4::ReferenceMap* refMap;
+    const P4::TypeMap* typeMap;
 
  public:
-    static Declaration create(const IR::PathExpression *pe, const P4::TypeMap *typeMap,
-                  const P4::ReferenceMap *refMap);
-    static Declaration create(const IR::Member *mem, const P4::TypeMap *typeMap,
-                  const P4::ReferenceMap *refMap);
+    GatherOutParamScalars(const P4::ReferenceMap *refMap_, const P4::TypeMap *typeMap_)
+        : refMap(refMap_), typeMap(typeMap_) {
+        CHECK_NULL(refMap_);
+        CHECK_NULL(typeMap_);
+    }
 
-    std::string toString() const;
+    std::unordered_set<const IR::IDeclaration*> get() const {
+        return b.get();
+    }
 
-private:
-    Declaration() = delete;
-    Declaration(const IR::IDeclaration *instance_) : instance(instance_) { CHECK_NULL(instance); }
-    Declaration(const IR::IDeclaration *instance_, std::vector<const IR::StructField *> field_)
-        : instance(instance_), field(std::move(field_)) {
-        CHECK_NULL(instance);
-        std::for_each(field->begin(), field->end(), [](auto* f) { CHECK_NULL(f); });
+ private:
+    bool preorder(const IR::PathExpression* pe) override {
+        if (!isWrite() || !findContext<IR::Argument>()) {
+            return true;
+        }
+        auto* type = typeMap->getType(pe);
+        if (isPrimitiveType(type)) {
+            CHECK_NULL(pe);
+            b.add(refMap->getDeclaration(pe->path));
+        }
+        return true;
     }
 };
 
 
-class GatherSSAReferences : public Inspector
+struct RefInfo {
+    std::variant<const IR::IDeclaration*, const IR::PathExpression*> ref;
+    // decl == ref for declarations
+    const IR::IDeclaration* decl;
+};
+
+
+class GatherSSAReferences : public Inspector, P4WriteContext
 {
     class Builder {
-        std::vector<Declaration> refs;
+        std::vector<RefInfo> reads;
+        std::vector<RefInfo> writes;
      public:
-        void add(Declaration decl) { refs.push_back(decl); }
-        std::vector<Declaration> get() const { return refs; };
+        void addRead(const IR::PathExpression* pe, const IR::IDeclaration* decl) {
+            CHECK_NULL(pe, decl);
+            reads.push_back({pe, decl});
+        }
+        void addRead(const IR::IDeclaration* decl) {
+             CHECK_NULL(decl);
+             reads.push_back({decl, decl});
+        }
+        void addWrite(const IR::PathExpression* pe, const IR::IDeclaration* decl) {
+            CHECK_NULL(pe, decl);
+            writes.push_back({pe, decl});
+        }
+        void addWrite(const IR::IDeclaration* decl) {
+             CHECK_NULL(decl);
+             writes.push_back({decl, decl});
+        }
+        std::vector<RefInfo> getReads() const { return reads; };
+        std::vector<RefInfo> getWrites() const { return writes; };
     };
 
     Builder b;
@@ -63,140 +104,37 @@ class GatherSSAReferences : public Inspector
     const P4::TypeMap* typeMap;
     const P4::ReferenceMap* refMap;
 
- public:
-    GatherSSAReferences(const P4::TypeMap *typeMap_, const P4::ReferenceMap *refMap_)
-        : typeMap(typeMap_), refMap(refMap_) {}
+    // Output of GatherOutParamScalars
+    const std::unordered_set<const IR::IDeclaration*> forbidden;
 
-    std::vector<Declaration> getSSARefs() const { return b.get(); }
+public:
+    std::vector<RefInfo> getReads() const { return b.getReads(); }
+    std::vector<RefInfo> getWrites() const { return b.getWrites(); }
 
  private:
     bool preorder(const IR::PathExpression* pe) override {
         auto* type = typeMap->getType(pe);
-        if (type && isPrimitiveType(type)) {
-            LOG3("GatherRefs: adding " << pe);
-            b.add(Declaration::create(pe, typeMap, refMap));
+        if (!isPrimitiveType(type)) {
+            return true;
+        }
+        CHECK_NULL(pe->path);
+        auto* decl = refMap->getDeclaration(pe->path);
+        if (forbidden.count(decl)) {
+            return true;
+        }
+        BUG_CHECK(!(isRead() && isWrite()), "InOut argument cannot be SSA value");
+        if (isRead()) {
+            b.addRead(pe, decl);
+        }
+        if (isWrite()) {
+            b.addRead(pe, decl);
         }
         return true;
     }
 
-    bool preorder(const IR::Member* mem) override {
-        // Field references of type
-        // <StructLike>.<StructLike>.<StructLike>.<PrimitiveType>
-        // can be converted into SSA values, since they cannot be aliased.
-        // Contrary to references like hdr.hs[idx].f1 or table.apply().hit.
-        auto cannotBeAliased = [&](const IR::Member* m) {
-            CHECK_NULL(m->expr);
-            const IR::Expression* ptr = m->expr;
-            while (ptr->is<IR::Member>()) {
-                auto* base = ptr->to<IR::Member>()->expr;
-                CHECK_NULL(base);
-                auto* type = typeMap->getType(base);
-                if (!type || !type->is<IR::Type_StructLike>()) {
-                    return false;
-                }
-                ptr = base;
-            }
-            auto* type = typeMap->getType(ptr);
-            return ptr->is<IR::PathExpression>() && type && type->is<IR::Type_StructLike>();
-        };
-
-        auto* type = typeMap->getType(mem);
-        if (type && isPrimitiveType(type) && cannotBeAliased(mem)) {
-            LOG3("GatherRefs: adding " << mem);
-            b.add(Declaration::create(mem, typeMap, refMap));
-        }
+    bool preorder(const IR::Declaration* decl) override {
+        b.addWrite(decl);
         return true;
-    }
-};
-
-
-class SSAConversion : public Transform
-{
-    // New statements/declarations that were created during visiting current statement/declaration.
-    // Gets copied into 'toInsert' and cleared after each statement.
-    std::vector<IR::StatOrDecl*> newStmts;
-
-    // New statements for current block statement. Maps insertion point to statements that need to
-    // be inserted before the insertion point.
-    std::unordered_map<const IR::StatOrDecl*, std::vector<IR::StatOrDecl*>> toInsert;
-
-    const P4::TypeMap* typeMap = nullptr;
-
- public:
-    SSAConversion(const P4::TypeMap* typeMap_) : typeMap(typeMap_) { CHECK_NULL(typeMap); }
-
- private:
-    IR::Node* preorder(IR::StatOrDecl* statOrDecl) {
-        newStmts.clear();
-        return statOrDecl;
-    }
-
-    IR::Node* postorder(IR::StatOrDecl* statOrDecl) {
-        if (!findContext<IR::BlockStatement>()) {
-            BUG_CHECK(newStmts.empty(), "Rewriting outside of block statement is not implemented");
-            return statOrDecl;
-        }
-        BUG_CHECK(!toInsert.count(statOrDecl), "Statement visited twice");
-        if (!newStmts.empty()) {
-            toInsert.insert({statOrDecl, std::move(newStmts)});
-            newStmts.clear();
-        }
-        return statOrDecl;
-    }
-
-    IR::Node* postorder(IR::MethodCallExpression* call) {
-        auto* baseType = typeMap->getType(call->method)->to<IR::Type_MethodBase>();
-        auto* retType = baseType->returnType;
-        bool hasRetVal = retType && !retType->is<IR::Type_Void>();
-        IR::Declaration_Variable* decl = nullptr;
-        if (hasRetVal && !getParent<IR::MethodCallStatement>()) {
-            // TODO: generate the name from refMap
-            static int id = 0;
-            std::string name = "__tmp" + std::to_string(id++);
-            decl = new IR::Declaration_Variable(IR::ID(name), retType);
-        }
-        if (decl) {
-            newStmts.push_back(decl);
-            auto* ref = new IR::PathExpression(decl->getName());
-            auto* refs = new IR::Vector<IR::Expression>({ref});
-            newStmts.push_back(new IR::SSACall(call, refs));
-            return new IR::PathExpression(decl->getName());
-        }
-        newStmts.push_back(new IR::SSACall(call, new IR::Vector<IR::Expression>()));
-        return nullptr;
-    }
-
-    IR::Node* postorder(IR::MethodCallStatement* stmt) {
-        BUG_CHECK(!newStmts.empty() && newStmts.back()->is<IR::SSACall>(),
-                  "MethodCallStatement should always produce SSACall");
-        auto* ssaCall = newStmts.back();
-        newStmts.pop_back();
-        BUG_CHECK(!toInsert.count(stmt), "Statement visited twice");
-        toInsert.insert({ssaCall, std::move(newStmts)});
-        newStmts.clear();
-        return ssaCall;
-    }
-
-    IR::Node* preorder(IR::BlockStatement* block) {
-        toInsert.clear();
-        return block;
-    }
-
-    IR::Node* postorder(IR::BlockStatement* block) {
-        if (toInsert.empty()) {
-            return block;
-        }
-        auto* newBlock = new IR::BlockStatement(block->srcInfo, block->annotations);
-        auto& components = newBlock->components;
-        for (auto* comp : block->components) {
-            if (toInsert.count(comp)) {
-                components.insert(components.end(), toInsert[comp].begin(), toInsert[comp].end());
-                toInsert.erase(comp);
-            }
-            components.push_back(comp);
-        }
-        BUG_CHECK(toInsert.empty(), "Some of the new statements were not inserted");
-        return newBlock;
     }
 };
 
--- extensions/mlir/tests/ssa.cpp
+++ extensions/mlir/tests/ssa.cpp
@@ -1,8 +1,10 @@
 #include "gtest/gtest.h"
 
+#include <unordered_set>
 #include "test/gtest/helpers.h"
 #include "frontends/common/parseInput.h"
 #include "frontends/common/resolveReferences/resolveReferences.h"
+#include "frontends/p4/toP4/toP4.h"
 #include "common.h"
 #include "../cfgBuilder.h"
 #include "../domTree.h"
@@ -15,147 +17,43 @@ namespace p4mlir::tests {
 class SSAInfo : public Test::P4CTest { };
 
 
-TEST_F(SSAInfo, Test_action_with_control_flow) {
+TEST_F(SSAInfo, Gather_out_param_scalars_collects_inout_and_out_args) {
     std::string src = P4_SOURCE(R"(
-        header Header {
-            bit<8> f1;
-            bit<8> f2;
-            bit<8> f3;
-            bit<8> f4;
-            bit<8> f5;
-            bit<8> f6;
-            bit<8> f7;
+        extern int<16> bar1(inout int<16> x1, in int<16> x2);
+        extern int<16> bar2(out int<16> x1);
+        action foo() {
+            int<16> f1 = 3;
+            int<16> f2 = 3;
+            int<16> f3 = 3;
+            int<16> f4 = 3;
+            bar1(f1, bar1(f2, f3));
+            bar2(f4);
         }
-        control Pipe<H>(inout H headers);
-        package Pipeline<H>(Pipe<H> p);
-        control TopPipe(inout Header hdr) {
-            int<16> label;
-            action foo() {
-                // bb1
-                label = 1;
-                if (hdr.f2 == hdr.f3) {
-                    // bb2
-                    label = 2;
-                    hdr.f1 = 3;
-                    if (hdr.f2 == hdr.f2) {
-                        // bb3
-                        label = 3;
-                        hdr.f2 = 3;
-                    } else {
-                        // bb4
-                        label = 4;
-                        hdr.f2 = 6;
-                    }
-                    // bb5
-                    label = 5;
-                    hdr.f6 = hdr.f1;
-                }
-                // bb6
-                label = 6;
-            }
-            apply {
-
-            }
-        }
-        Pipeline(TopPipe()) main;
     )");
     auto* program = P4::parseP4String(src, CompilerOptions::FrontendVersion::P4_16);
     ASSERT_TRUE(program && ::errorCount() == 0);
 
-    // auto b = new p4mlir::CFGBuilder;
-    // program->apply(*b);
-    // auto cfg = b->getCFG();
-
     auto* refMap = new P4::ReferenceMap();
     auto* typeMap = new P4::TypeMap();
-    program->apply(P4::ResolveReferences(refMap));
-    program->apply(P4::TypeInference(refMap, typeMap, false, true));
-    ASSERT_TRUE(program != nullptr && ::errorCount() == 0);
+    program = program->apply(P4::ResolveReferences(refMap));
+    program = program->apply(P4::TypeInference(refMap, typeMap, false, true));
+    program = program->apply(P4::TypeChecking(refMap, typeMap, true));
+    ASSERT_TRUE(program && ::errorCount() == 0);
 
-    // auto* cfgFoo = getByName(cfg, "foo");
+    auto* g = new p4mlir::GatherOutParamScalars(refMap, typeMap);
+    program->apply(*g);
+    ASSERT_TRUE(program && ::errorCount() == 0);
 
-    // const p4mlir::DomTree* domTree = p4mlir::DomTree::fromEntryBlock(cfgFoo);
-    // const p4mlir::SSAInfo* ssa = p4mlir::SSAInfo::create(cfgFoo, domTree, refMap);
-    // ASSERT_TRUE(ssa);
-
-    // p4mlir::BasicBlock* bb1 = getByStmtString(cfgFoo, "label = 1;");
-    // p4mlir::BasicBlock* bb2 = getByStmtString(cfgFoo, "label = 2;");
-    // p4mlir::BasicBlock* bb3 = getByStmtString(cfgFoo, "label = 3;");
-    // p4mlir::BasicBlock* bb4 = getByStmtString(cfgFoo, "label = 4;");
-    // p4mlir::BasicBlock* bb5 = getByStmtString(cfgFoo, "label = 5;");
-    // p4mlir::BasicBlock* bb6 = getByStmtString(cfgFoo, "label = 6;");
-
-    auto* gatherRefs = new p4mlir::GatherSSAReferences(typeMap, refMap);
-    program->apply(*gatherRefs);
-
-    auto refs = gatherRefs->getSSARefs();
-    for (auto& r : refs) {
-        std::cout << r.toString() << std::endl;
-    }
-
-    /*std::string src = P4_SOURCE(R"(
-        action foo() {
-            // bb1
-            label = 1;
-            if (hdr.f2 == hdr.f3 + 1) {
-                // bb2
-                label = 2;
-                hdr.f1 = 3;
-                if (hdr.f2 == hdr.f2 + 1) {
-                    // bb3
-                    label = 3;
-                    hdr.f2 = 3;
-                } else {
-                    // bb4
-                    label = 4;
-                    hdr.f2 = 6;
-                }
-                // bb5
-                label = 5;
-                hdr.f6 = hdr.f1;
-            }
-            // bb6
-            label = 6;
-            test.apply();
-            return hdr.f1, hdr.f2, hdr.f6
+    auto names = [](std::unordered_set<const IR::IDeclaration*> decls) {
+        std::unordered_set<cstring> res;
+        for (auto* d : decls) {
+            res.insert(d->getName().name);
         }
-    )");*/
+        return res;
+    };
 
-        // need top sort on call graph!!!
-        // must know all writes and reads including called actions
-        // from each statement i need writes and reads (member, pathexpression, )
-        // extern objects cannot be ssa, they can be invalidated through use of another extern method/function (unless annotated with noSideEffect)
-        // 1. analyze/tranform functions - we need to know what values they define, use
-        //      - transform function prototypes
-        //      - transform all call sites
-        /*std::string src = P4_SOURCE(R"(
-        action foo() {
-            // bb1
-            label-1 = 1;
-            if (hdr.f2-1 == hdr.f3-1 + 1) {
-                // bb2
-                label-2 = 2;
-                hdr.f1$2 = 3;
-                if (hdr.f2$1 == hdr.f2$1 + 1) {
-                    // bb3
-                    label$4 = 3;
-                    hdr.f2$2 = 3;
-                } else {
-                    // bb4
-                    label$5 = 4;
-                    hdr.f2$3 = 6;
-                }
-                // bb5
-                // phis
-                label$6 = 5;
-                hdr.f6$2 = hdr.f1$2;
-            }
-            // bb6
-            label$7 = 6;
-                                __ __ __ __ = test.apply(__ __ __ __ __);
-            return hdr.f1, hdr.f2, hdr.f6
-        }
-    )");*/
+    using unordered = std::unordered_set<cstring>;
+    EXPECT_EQ(names(g->get()), unordered({"f1", "f2", "f4"}));
 }
 
 
